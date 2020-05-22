ff35fc4d1f15435bdb996bd71f4051a52300a316
YouruiGuo <yourui@ualberta.ca>
2020-05-21 18:16:34 -0600

    dj fixed

--- dj_contro_client.py
+++ dj_contro_client.py
@@ -6,14 +6,14 @@ URL = 'https://webdocs.cs.ualberta.ca/~yourui/djcontrol_server.py'
 current_url = 'https://webdocs.cs.ualberta.ca/~yourui/currentplaying.py'
 
 res = requests.post(url=current_url)
-uuid = re.findall(r'\n([^|\n]*)\n', res.content)
+uuid = re.findall(r'\n([^|\n]*)\n', res.content.decode('utf-8'))
 
 while True:
 	song = input('enter the song number (0-4):')
 	volume = input('enter the volume number (0-7):')
 	data = {}
-	data['song'] = song
-	data['volume'] = volumes[volume]
+	data['song'] = str(song)
+	data['volume'] = str(volumes[int(volume)])
 	data['uuid'] = uuid
 	print(data)
 	r = requests.get(url = URL, params = data)
